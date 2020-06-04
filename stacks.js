81752ef7c557080bff7ecf1013ffca06f893cafd
Anya <annagapyuk@gmail.com>
2020-06-04 11:29:35 -0400

    added another example implementing stacks

--- stacks.js
+++ stacks.js
@@ -26,3 +26,51 @@ if (rword === word) {
     alert(word + " is not a pallindrome");
 }
 
+// another examples implementing stacks
+// I can use arrays as stack
+
+// creates a stack
+var Stack = function() {
+this.count = 0;
+this.storage = {};
+
+// adds a value onto the end of the stack
+this.push = function(value) { // passing the value only when pushing
+    this.storage[this.count] = value;
+    this.count++;
+    }
+
+    // removes and returns the value at the end of the stack
+    this.pop = function() {
+        if (this.count === 0) {
+            return undefined;
+        }
+
+        this.count--;
+        var result = this.storage[this.count];
+        delete this.storage[this.count];
+        return result;
+    }
+
+    this.size = function() {
+        return this.count;
+    }
+
+    // returns the value at the end of the stack
+    this.peek = function() {
+        return this.storage[this.count-1];
+    }
+}
+
+var myStack = new Stack();
+
+myStack.push(1);
+myStack.push(2);
+alert(myStack.peek());
+alert(myStack.pop());
+alert(myStack.peek());
+myStack.push("freeCodeCamp");
+alert(myStack.size());
+alert(myStack.peek());
+alert(myStack.pop());
+alert(myStack.peek());
