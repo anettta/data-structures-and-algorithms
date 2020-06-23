2339326dc01b46e4b20994313944a6cb53f5dfb2
Anya <annagapyuk@gmail.com>
2020-06-23 11:43:08 -0400

    added comment

--- sets.js
+++ sets.js
@@ -4,18 +4,18 @@
 
 function mySet() {
     // the var collection will hold the set
-    var collection = [];
+    var collection = []; // no duplicates
     // this method will check for the presence of an element and return true or false
     this.has = function(element) {
         return (collection.indexOf(element) !== -1); // returns true or false(-1)
     };
-    // this method will return all the values in the set
+    // this method will return all the values of the set
     this.values = function() {
         return collection;
     };
     // this method will add an element to the set
     this.add = function(element) {
-        if(!this.has(element)) { //checks if collection has that element
+        if(!this.has(element)) { // has checks if collection has that element
             collection.push(element);
             return true;
         }
